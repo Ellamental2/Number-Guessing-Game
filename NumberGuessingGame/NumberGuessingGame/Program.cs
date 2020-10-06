ca7f93b13f50ecb43b16040eb0bab62eb6e68e4a
Ellamental2 <43385250+Ellamental2@users.noreply.github.com>
2020-10-06 23:17:53 +0100

    Finalised Comments
    I finalised the comments as i noticed some issues where i had not updated the comments. And i updated the switch statement.

--- NumberGuessingGame/NumberGuessingGame/Program.cs
+++ NumberGuessingGame/NumberGuessingGame/Program.cs
@@ -7,20 +7,20 @@ namespace NumberGuessingGame
 
         static void Main(string[] args)
         {
-            //run through the game at least once.
+            // Run through the game at least once.
             do
             {
-                //Clear console.
+                // Clear console.
                 Console.Clear();
 
                 #region public values
-                // the lowest possible value - 1
-                var lowerBound = 1;
+                // Lowest possible value
+                var lowerBound = 0;
 
-                // WHighest Number + 1
+                // Highest posible value
                 var upperBound = 100;
 
-                //declare userGuess as a number outside of the possible range.
+                // Declare userGuess as a number outside of the possible range.
                 var correct = false;
 
                 // Create random number
@@ -31,21 +31,21 @@ namespace NumberGuessingGame
                 //Say Hi
                 Greeting();
 
-                //repeat while the user is incorrect
+                // Repeat while the user is incorrect
                 while (!correct)
                 {
-                    //ask the user to guess and store in a variable.
+                    // Ask the user to guess and store in a variable.
                     var userGuess = UserGuess(lowerBound, upperBound);
 
-                    //inform the user if they are higher, lower or correct.
+                    // Inform the user if they are higher, lower or correct.
                     correct = IsGuessCorrect(userGuess, target, ref lowerBound, ref upperBound);
                 }
 
 
-                // if the player wants to play again, we return to the start of the game.
+                // If the player wants to play again, we return to the start of the game.
             } while (PlayAgain());
 
-            //thank the player for playing.
+            // Thank the player for playing.
             ThanksForPlaying();
         }
 
@@ -54,13 +54,13 @@ namespace NumberGuessingGame
         /// </summary>
         private static void Greeting()
         {
-            //explains the game to the user
+            // Explains the game to the user
             Console.WriteLine("Hi There! Welcome to my Guessing Game");
             Console.WriteLine("I will think of a number between 1 and 100 and you need to try and guess the number!");
             Console.WriteLine("If you guess is incorrect, I'll give you a little hint.");
             Console.WriteLine("Press any key to start");
 
-            //pauses the application until the user inputs a keystroke
+            // Pauses the application until the user inputs a keystroke
             Console.ReadKey();
         }
 
@@ -73,20 +73,20 @@ namespace NumberGuessingGame
         /// <returns></returns>
         private static int UserGuess(int lowerBound, int upperBound)
         {
-            //do until the user guesses a valid number
+            // Do until the user guesses a valid number
             while (true)
             {
-                //asks the user to guess a number between the range
+                // Asks the user to guess a number between the range
                 Console.WriteLine($"I am thinking of a number between {lowerBound} and {upperBound}. Can you Guess What it is?");
 
-                //save the users response as a string
+                // Save the users response as a string
                 var userResponse = Console.ReadLine();
 
-                //If the user's response can be turned into a number within the given range...
+                // If the user's response can be turned into a number within the given range...
                 if (int.TryParse(userResponse, out int userGuess) && userGuess <= upperBound && userGuess >= lowerBound)
-                    //return the user's guess
+                    // return the user's guess
                     return userGuess;
-                //Otherwise
+                // Otherwise
                 else
                     // Inform the user that their guess was a number not within the range
                     Console.WriteLine($"Your guess was {userResponse}. This is not a whole number between {lowerBound} and {upperBound}.");
@@ -104,33 +104,33 @@ namespace NumberGuessingGame
         /// <returns></returns>
         private static bool IsGuessCorrect(int userGuess, int target, ref int lowerBound, ref int upperBound)
         {
-            //if the user guess is higher than the target number...
+            // If the user guess is higher than the target number...
             if (userGuess > target)
             {
-                // set the upperBound to the user's guess - 1
+                // Set the upperBound to the user's guess - 1
                 upperBound = userGuess - 1;
-                // tell the user they were incorrect
+                // Tell the user they were incorrect
                 Console.WriteLine($"You guessed {userGuess}. This was higher than the number I'm thinking of.");
-                //return false
+                // Return false
                 return false;
             }
             // If the user guess is lower than the target number...
             else if (userGuess < target)
             {
 
-                // set the lowerBound number to the user's guess + 1
+                // Set the lowerBound number to the user's guess + 1
                 lowerBound = userGuess + 1;
-                //tell the user they were incorrect
+                // Tell the user they were incorrect
                 Console.WriteLine($"You guessed {userGuess}. This was lower than the number I'm thinking of.");
-                //return false
+                // Return false
                 return false;
             }
-            //If we have made it to here then the user is correct
+            // If we have made it to here then the user is correct
             else
             {
-                //inform the user they are correct
+                // Inform the user they are correct
                 Console.WriteLine($"You guessed {userGuess}... THIS IS CORRECT!! WELL DONE!!");
-                //return true
+                // Return true
                 return true;
             }
         }
@@ -142,20 +142,34 @@ namespace NumberGuessingGame
         /// <returns> Whether or not the player wants to play again. </returns>
         private static bool PlayAgain()
         {
-            //create a continuous loop until a valid iput is received
+            // Create a continuous loop until a valid iput is received
             while (true)
             {
+                // Ask the user if they want to play again
                 Console.WriteLine("Would you like to play Again? Y/N");
 
+                // Save the users response
                 var userResponse = Console.ReadLine();
 
                 switch (userResponse)
                 {
+                    // If the user said yes
                     case "Y":
+                    case "y":
+                    case "yes":
+                    case "Yes":
+                        // Return true
                         return true;
+                    // If the user said no
                     case "N":
+                    case "n":
+                    case "no":
+                    case "No":
+                        // Return false
                         return false;
+                    // If the input is not recognised
                     default:
+                        // Inform the user that the response is not understood.
                         Console.WriteLine($"I'm sorry, I don't understand your response: {userResponse}");
                         break;
                 }
@@ -168,10 +182,11 @@ namespace NumberGuessingGame
         /// </summary>
         private static void ThanksForPlaying()
         {
-            //thank the user
+            // Thank the user
             Console.WriteLine("Thankyou for playing!");
+            System.Console.WriteLine("Press any key to exit.");
 
-            //pause until the user enters a keystroke
+            // Pause until the user enters a keystroke
             Console.ReadKey();
         }
 
